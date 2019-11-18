c5a2be08a36124bb4d3aa69b73fcff8443d3f997
Carlos Rueda <carueda@mbari.org>
2019-11-18 11:14:24 -0800

    move markdown style to component

--- src/components/mxm-markdown.vue
+++ src/components/mxm-markdown.vue
@@ -42,3 +42,37 @@
     },
   }
 </script>
+
+<style>
+  .markdownText {
+    white-space: normal !important;
+    line-height: 1.5;
+  }
+</style>
+
+<!-- https://divtable.com/table-styler/ -->
+<style>
+  table.markdownTable {
+    border: 1px solid #1C6EA4;
+    /*width: 100%;*/
+    text-align: left;
+    border-collapse: collapse;
+  }
+  table.markdownTable td, table.markdownTable th {
+    border: 1px solid #AAAAAA;
+    padding: 3px 8px;
+  }
+  table.markdownTable tbody td {
+    font-size: 13px;
+  }
+  table.markdownTable thead {
+    /*background: #cfe2ff;*/
+  }
+  table.markdownTable thead th {
+    font-size: 14px;
+    border-left: 2px solid #D0E4F5;
+  }
+  table.markdownTable thead th:first-child {
+    border-left: none;
+  }
+</style>
